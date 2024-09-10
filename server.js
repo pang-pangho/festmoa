dc95f271484eb6666184f59f8420c918104b2b40
pangho <pang-pangho>
2024-09-10 20:32:46 +0900

    API KEY 숨김

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-// require("dotenv").config(); // .env 파일을 로드합니다.
-
-// const express = require("express");
-// const axios = require("axios");
-// const converter = require("xml-js");
-// const app = express();
-// const cors = require("cors");
-
-// app.use(cors());
-
-// const API_KEY = "eb5b6953c1e248a39d718ba525535aa1"; // .env 파일에서 API 키를 가져옵니다.
-// const url = `http://www.kopis.or.kr/openApi/restful/prffest?service=${API_KEY}&stdate=20240505&eddate=20241220&cpage=1&rows=10&shcate=CCCD
-// `;
-// console.log(url);
-
-// app.get("/api", async (req, res) => {
-//   try {
-//     const response = await axios.get(url);
-//     const jsonResult = converter.xml2json(response.data, {
-//       compact: true,
-//       spaces: 2,
-//     });
-//     res.json(JSON.parse(jsonResult));
-//   } catch (error) {
-//     console.error("Error during API request:", error);
-//     res.status(500).json({ error: "API 요청 실패", details: error.message });
-//   }
-// });
-
-// const port = 5000;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 require("dotenv").config(); // 환경 변수 로드
 
 const express = require("express");
