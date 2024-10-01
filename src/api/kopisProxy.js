841695679ccad91d3cd757a60be20fefc666f250
pangho <pang-pangho>
2024-10-01 21:05:55 +0900

    .

--- src/api/kopisProxy.js
+++ src/api/kopisProxy.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 export default async function handler(req, res) {
   const { query } = req; // 요청된 쿼리 파라미터를 가져옵니다.
   const API_KEY = process.env.REACT_APP_KOPIS_API_KEY;
-  const API_URL = `https://kopis.or.kr/openApi/restful/pblprfr?service=${API_KEY}&${new URLSearchParams(
-    query
-  ).toString()}`;
+  const API_URL = `https://kopis.or.kr/openApi/restful/pblprfr?service=${
+    process.env.REACT_APP_KOPIS_API_KEY
+  }&${new URLSearchParams(query).toString()}`;
 
   try {
     const response = await axios.get(API_URL); // KOPIS API에 요청을 보냅니다.
