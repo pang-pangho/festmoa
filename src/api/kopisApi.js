7c93f8628d07253ee10d5dc91dfab013a14a1822
pangho <pang-pangho>
2024-10-01 20:14:04 +0900

    .

--- src/api/kopisApi.js
+++ src/api/kopisApi.js
@@ -6,7 +6,7 @@ const baseURL =
   process.env.NODE_ENV === "production"
     ? "/openApi/restful/"
     : "http://localhost:3000/openApi/restful"; // 로컬 테스트 시에는 KOPIS API를 직접 호출
-console.log(process.env.NODE_EN);
+console.log(process.env.NODE_ENV);
 const API_KEY = process.env.REACT_APP_KOPIS_API_KEY;
 
 const kopisApi = axios.create({
