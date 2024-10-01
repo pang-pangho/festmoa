53ef7a9c6075e888e0841118f235cccf29a8deb6
pangho <pang-pangho>
2024-10-01 20:22:22 +0900

    ..

---
+++ netlify/functions/kopisProxy.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+exports.handler = async function (event) {
+  const { queryStringParameters } = event;
+  const API_KEY = process.env.REACT_APP_KOPIS_API_KEY;
+  const API_URL = `https://kopis.or.kr/openApi/restful/pblprfr?service=${API_KEY}&${new URLSearchParams(
+    queryStringParameters
+  ).toString()}`;
+
+  try {
+    const response = await axios.get(API_URL);
+    return {
+      statusCode: 200,
+      body: response.data,
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response?.status || 500,
+      body: error.message,
+    };
+  }
+};
