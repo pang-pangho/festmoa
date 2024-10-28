f617b68f50d5ec0c8a0f76f2f88daf49ddb107cf
pangho <pang-pangho>
2024-10-28 15:35:34 +0900

    cors.sh추가

--- src/api/kopisApi.js
+++ src/api/kopisApi.js
@@ -4,16 +4,21 @@ import { convertXmlToJson } from "../utils/xmlToJson";
 // 환경 변수에서 baseURL 가져오기
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "/api/kopisProxy"
+    ? "https://proxy.cors.sh/https://kopis.or.kr/openApi/restful"
     : "http://localhost:3000/openApi/restful"; // 로컬 테스트 시에는 KOPIS API를 직접 호출
 
 console.log(process.env.NODE_ENV);
 const API_KEY = process.env.REACT_APP_KOPIS_API_KEY;
+const CORS_API_KEY = "temp_5eb2645376d1df14021a05db0b16c3a5"; // 발급받은 cors.sh API 키
 console.log("API Key:", API_KEY); // API 키 확인
 console.log("Base URL:", baseURL); // baseURL 확인
+
 const kopisApi = axios.create({
   baseURL,
-  params: { service: process.env.REACT_APP_KOPIS_API_KEY },
+  params: { service: API_KEY },
+  headers: {
+    "x-cors-api-key": CORS_API_KEY, // cors.sh API 키 헤더에 추가
+  },
 });
 
 // 공연 목록을 가져오는 함수
@@ -30,27 +35,16 @@ export const fetchPerformances = async (params) => {
 };
 
 // 공연 상세 정보를 가져오는 함수
-// 공연 상세 정보를 가져오는 함수
 export const fetchPerformanceDetails = async (performanceId) => {
   try {
-    // 공연 ID를 콘솔에 출력해서 제대로 전달되고 있는지 확인
     console.log("Fetching details for performance ID:", performanceId);
-
-    // 요청 URL에 공연 ID를 정확하게 넣어서 요청
     const response = await kopisApi.get(`/pblprfr/${performanceId}`, {
       params: { service: API_KEY },
     });
-
-    // 응답 결과를 콘솔에 출력해서 확인
     console.log("Performance details response:", response.data);
-
-    // XML을 JSON으로 변환하여 반환
     const jsonResult = convertXmlToJson(response.data);
-
-    // 변환된 JSON 결과를 반환
     return jsonResult;
   } catch (error) {
-    // 에러 발생 시 에러 메시지를 출력하고 throw
     console.error("Performance details fetching failed:", error.message);
     throw error;
   }
